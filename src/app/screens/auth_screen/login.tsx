348974e4ad312242a05a51aa455e804713cb6173
Lovish Hamal <lovishhamal123@gmail.com>
2020-09-17 15:37:55 +0545

    apple login

--- src/app/screens/auth_screen/login.tsx
+++ src/app/screens/auth_screen/login.tsx
@@ -1,18 +1,40 @@
-import React from "react";
-import { Text, View, Image } from "react-native";
-import { useNavigation } from "@react-navigation/native";
-import { TouchableWithoutFeedback } from "react-native-gesture-handler";
-import { loginStyle as style } from "../../../assets/styles";
+import React from 'react';
+import { Text, View, Image, TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
+import { loginStyle as style } from '../../../assets/styles';
+
+import appleAuth, {
+  AppleButton,
+  AppleAuthRequestOperation,
+  AppleAuthRequestScope,
+  AppleAuthCredentialState,
+} from '@invertase/react-native-apple-authentication';
 
 const Login = () => {
   const navigation = useNavigation();
+
+  const signInApple = async () => {
+    return appleAuth
+      .performRequest({
+        requestedOperation: AppleAuthRequestOperation.LOGIN,
+        requestedScopes: [
+          AppleAuthRequestScope.EMAIL,
+          AppleAuthRequestScope.FULL_NAME,
+        ],
+      })
+      .then((res) => {
+        let { identiyToken, email } = res;
+        console.log('res -> ', res);
+      });
+  };
 
   return (
     <View style={style.container}>
       <View style={style.imageView}>
         <Image
           style={style.image}
-          source={require("../../../assets/images/noveltylogo.png")}
+          source={require('../../../assets/images/noveltylogo.png')}
         />
         <Text style={style.imageText}>Novelty EMS</Text>
       </View>
@@ -22,14 +44,17 @@ const Login = () => {
           <View style={style.iconView}>
             <Image
               style={style.icon}
-              source={require("../../../assets/images/icons8-google-96.png")}
+              source={require('../../../assets/images/icons8-google-96.png')}
             />
           </View>
+
           <View style={style.iconView}>
-            <Image
-              style={style.icon}
-              source={require("../../../assets/images/icons8-apple-logo-100.png")}
-            />
+            <TouchableOpacity onPress={() => signInApple()}>
+              <Image
+                style={style.icon}
+                source={require('../../../assets/images/icons8-apple-logo-100.png')}
+              />
+            </TouchableOpacity>
           </View>
         </View>
       </View>
