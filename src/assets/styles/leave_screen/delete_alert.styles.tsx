5ff59f858cc94e01aec09160fb001f21655d9862
Lovish Hamal <lovishhamal123@gmail.com>
2020-09-21 17:37:30 +0545

    model styling changed

--- src/assets/styles/leave_screen/delete_alert.styles.tsx
+++ src/assets/styles/leave_screen/delete_alert.styles.tsx
@@ -1,17 +1,58 @@
-import { StyleSheet } from "react-native";
-import normalize from "react-native-normalize";
-import colors from "../../colors";
+import { Platform, StyleSheet } from 'react-native';
+import normalize from 'react-native-normalize';
+import colors from '../../colors';
 
 const deleteAlertStyle = StyleSheet.create({
-  container: {
-    flexDirection: "row",
-    paddingBottom: normalize(30),
+  dialogContainer: {
+    width: '92%',
+    borderRadius: 4,
   },
-  gap: {
-    width: normalize(10),
+  container: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingBottom: normalize(20),
+    paddingHorizontal: normalize(15),
+  },
+  main: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'flex-start',
+    marginLeft: normalize(10),
+  },
+  text1: {
+    fontSize: normalize(19),
+  },
+  text2: {
+    fontSize: normalize(13),
+    color: colors.fontGrey,
+  },
+  buttons: {
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+
+    ...Platform.select({
+      ios: {
+        paddingTop: normalize(10),
+        paddingBottom: normalize(25),
+      },
+      android: {
+        paddingTop: normalize(5),
+        paddingBottom: normalize(5),
+      },
+    }),
   },
   cancel: {
     color: colors.black,
+    fontSize: normalize(15),
+    ...Platform.select({
+      ios: {
+        marginLeft: normalize(110),
+      },
+      android: {
+        marginRight: normalize(10),
+      },
+    }),
   },
   delete: {
     color: colors.primary,
