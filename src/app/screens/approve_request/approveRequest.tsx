6f030b19279e13e87ebd964c2e50a1d9fd3dc0c5
Lovish Hamal <lovishhamal123@gmail.com>
2020-09-22 10:21:03 +0545

    added approve request component

---
+++ src/app/screens/approve_request/approveRequest.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Text } from 'react-native';
+import { approveRequest as style } from '../../../assets/styles';
+
+const approveRequest = () => {
+  return <Text>Approve Request</Text>;
+};
+
+export default approveRequest;
---
+++ src/assets/styles/approve_request/approve_request.styles.tsx
@@ -0,0 +1,6 @@
+import { StyleSheet } from 'react-native';
+import colors from '../../colors';
+
+const approveRequest = StyleSheet.create({});
+
+export { approveRequest };
