d316d808aaa74772dcbe5803c3a9a2e1edd5e2be
rajad shakya <rajad@noveltytechnology.com>
2020-09-25 14:04:39 +0545

    tab navigation added

--- src/app/navigations/index.tsx
+++ src/app/navigations/index.tsx
@@ -1,19 +1,59 @@
-import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
-import ScreenStack from './screenStack';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import ScreenStack from "./screenStack";
+import colors from "../../assets/colors";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 
-const RootStack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 
 const RootNavigation = () => {
   return (
     <NavigationContainer>
-      <RootStack.Navigator
-        screenOptions={{
-          headerShown: false,
-        }}>
-        <RootStack.Screen name="screen" component={ScreenStack} />
-      </RootStack.Navigator>
+      <Tab.Navigator
+        tabBarOptions={{
+          activeTintColor: colors.primary,
+          showLabel: false,
+        }}
+      >
+        <Tab.Screen
+          name="Home"
+          component={ScreenStack}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <Icon name="home" color={color} size={size} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Activity"
+          component={ScreenStack}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <Icon name="timer" color={color} size={size} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Profile"
+          component={ScreenStack}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <Icon name="account" color={color} size={size} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Setting"
+          component={ScreenStack}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <Icon name="cog" color={color} size={size} />
+            ),
+          }}
+        />
+      </Tab.Navigator>
+      {/* <ScreenStack /> */}
     </NavigationContainer>
   );
 };
