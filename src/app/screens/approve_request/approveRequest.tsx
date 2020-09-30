ddd3a1e8fe595414ae680c6a8b344ab2cd93acb6
Lovish Hamal <lovishhamal123@gmail.com>
2020-09-30 21:30:10 +0545

    change in approve

--- src/app/screens/approve_request/approveRequest.tsx
+++ src/app/screens/approve_request/approveRequest.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text } from 'react-native';
 import { header as Header } from '../../common/header';
 import { headerText } from '../../../assets/styles';
@@ -8,39 +8,50 @@ import { button as Button } from '../../common';
 import Request from '../../components/approveRequest/approve_request';
 import getDay from '../../components/approveRequest/getDay';
 import getName from '../../components/approveRequest/getName';
+import ResponseModal from '../../components/approveRequest/responseModal';
 
 const ApproveRequest = ({ route }: any) => {
-  const { first_name, last_name } = route.params.user;
+  const [showAlert, setShowAlert] = useState(false);
 
   let { dayRange, day } = getDay(route.params);
   let { name } = getName(route.params);
 
-  const Approve = (res) => {
-    route.params['state'] = res;
-  };
-  const Deny = (res) => {
-    route.params['state'] = res;
-  };
-
   return (
     <>
       <Header>
-        <Text style={headerText}>{name},</Text>
-        <Text style={style.headerDate}>
-          {dayRange} ({day > 1 ? day + ' days' : (day = 1 + ' day')})
-        </Text>
+        <View
+          style={{
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}
+        >
+          <Text>
+            <Text style={headerText}>{name},</Text>
+          </Text>
+          <Text>
+            <Text style={style.headerDate}>
+              {dayRange} ({day > 1 ? day + ' days' : (day = 1 + ' day')})
+            </Text>
+          </Text>
+        </View>
       </Header>
       <Request data={route.params} style={style} />
+      <ResponseModal
+        item={route.params}
+        showAlert={showAlert}
+        setShowAlert={setShowAlert}
+      />
       <View style={style.buttonView}>
         <Button
           title="Approve"
           style={style.buttonApprove}
-          onPress={() => Approve('Approved')}
+          onPress={() => setShowAlert(!showAlert)}
         />
         <Button
           title="Deny"
           style={style.buttonDeny}
-          onPress={() => Deny('Denied')}
+          onPress={() => setShowAlert(!showAlert)}
         />
       </View>
     </>
