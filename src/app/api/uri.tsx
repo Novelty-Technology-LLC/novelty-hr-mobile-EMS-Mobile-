441ac6a53fa1627581d42b0f9a661b16fd5cef78
Lovish Hamal <lovishhamal123@gmail.com>
2020-10-12 17:30:28 +0545

    merge

--- src/app/api/uri.tsx
+++ src/app/api/uri.tsx
@@ -2,4 +2,4 @@ import { Platform } from 'react-native';
 export const BASE_URI =
   Platform.OS === 'ios'
     ? 'http://127.0.0.1:8080'
-    : 'http://7dd44114407d.ngrok.io';
+    : 'http://10.0.2.2:8080';
