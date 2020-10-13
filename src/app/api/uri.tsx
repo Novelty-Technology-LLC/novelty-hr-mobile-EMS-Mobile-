e0bbd053bebc005433dd8dc83799b983daa99163
rj7shakya <shakyarajad1@gmail.com>
2020-10-13 16:57:11 +0545

    some change

--- src/app/api/uri.tsx
+++ src/app/api/uri.tsx
@@ -1,5 +1,1 @@
-import { Platform } from 'react-native';
-export const BASE_URI =
-  Platform.OS == 'ios'
-    ? 'http://127.0.0.1:8088'
-    : 'https://2f51f99fbcf3.ngrok.io';
+export const BASE_URI = 'https://novelty-hr-api.herokuapp.com';
