8fb702da6d6d8f7b38ba7f37152395b623c8ffb3
Lovish Hamal <lovishhamal123@gmail.com>
2020-10-15 15:42:56 +0545

    sign in success

--- src/app/utils/googleConfig.tsx
+++ src/app/utils/googleConfig.tsx
@@ -4,9 +4,9 @@ const GoogleConfig = () =>
   GoogleSignin.configure({
     scopes: ['https://www.googleapis.com/auth/drive.readonly'], // what API you want to access on behalf of the user, default is email and profile
     webClientId:
-      '742877336730-9lt7k06p7eihbb6rar2esdeal9recl2l.apps.googleusercontent.com', // client ID of type WEB for your server (needed to verify user ID and offline access)
+      '999740786889-dd4e9nd4ifq1md20n32lum3ajago6ckd.apps.googleusercontent.com', // client ID of type WEB for your server (needed to verify user ID and offline access)
     iosClientId:
-      '742877336730-8mscg3mk8ccel2u3gkr7njhq3g9jod86.apps.googleusercontent.com',
+      '999740786889-leks6sl2o6nkguudgndbs0hsfrcrmsmg.apps.googleusercontent.com',
     offlineAccess: true, // if you want to access Google API on behalf of the user FROM YOUR SERVER
     forceCodeForRefreshToken: true, // [Android] related to `serverAuthCode`, read the docs link below *.
   });
