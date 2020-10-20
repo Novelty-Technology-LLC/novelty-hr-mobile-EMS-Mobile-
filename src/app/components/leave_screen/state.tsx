217c79da872a3fd09839f202ba79610ffd20ab45
Lovish Hamal <lovishhamal123@gmail.com>
2020-10-20 20:47:38 +0545

    styling changed

--- src/app/components/leave_screen/state.tsx
+++ src/app/components/leave_screen/state.tsx
@@ -25,7 +25,6 @@ const State = ({ state, children }: { state: string; children?: any }) => {
         <>
           <View style={style.main}>
             <View style={style.wrapper}>
-              <View style={style.progress}>
                 <AppIcon
                   name="timer-sand"
                   size={Platform === 'ios' ? 10 : 12}
@@ -34,7 +33,6 @@ const State = ({ state, children }: { state: string; children?: any }) => {
               </View>
               <View style={style.space}></View>
               <Text style={style.state}>{state}</Text>
-            </View>
           </View>
         </>
       )}
