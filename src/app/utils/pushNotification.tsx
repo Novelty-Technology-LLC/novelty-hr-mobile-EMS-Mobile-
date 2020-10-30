6382e77acb336de6db3a52f4735bd20bcc39f68a
Lovish Hamal <lovishhamal123@gmail.com>
2020-10-30 15:41:23 +0545

    update

--- src/app/utils/pushNotification.tsx
+++ src/app/utils/pushNotification.tsx
@@ -4,19 +4,7 @@ import { View, StyleSheet, Button, Alert } from 'react-native';
 PushNotification.configure({
   // (required) Called when a remote or local notification is opened or received
   onNotification: function (notification) {
-    Alert.alert(
-      'Alert Title',
-      'My Alert Msg',
-      [
-        {
-          text: 'Cancel',
-          onPress: () => console.log('Cancel Pressed'),
-          style: 'cancel',
-        },
-        { text: 'OK', onPress: () => console.log('OK Pressed') },
-      ],
-      { cancelable: false }
-    );
+    console.log('notif recieved -> ');
   },
 });
 
@@ -24,10 +12,7 @@ export const SetLocalNotification = () => {
   checkPermission();
   PushNotification.localNotification({
     autoCancel: true,
-    bigText: 'Mobile utility push notification demo.',
-    subText: 'Remote notification',
-    title: 'Mobilie utility',
-    message: 'Expand me to see more',
+    message: 'You have got a new request',
     vibrate: true,
     vibration: 300,
     playSound: true,
