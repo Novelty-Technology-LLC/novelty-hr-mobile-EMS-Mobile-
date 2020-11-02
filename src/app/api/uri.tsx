67f1b0def6532e69c4488d88d7b1f31e04a71207
rj7shakya <shakyarajad1@gmail.com>
2020-11-02 14:10:29 +0545

    uri changes

--- src/app/api/uri.tsx
+++ src/app/api/uri.tsx
@@ -1,2 +1,2 @@
-// export const BASE_URI = 'https://novelty-hr-api.herokuapp.com/';
-export const BASE_URI = 'https://940827b6cbee.ngrok.io';
+export const BASE_URI = 'https://novelty-hr-api.herokuapp.com/';
+// export const BASE_URI = 'https://940827b6cbee.ngrok.io';
