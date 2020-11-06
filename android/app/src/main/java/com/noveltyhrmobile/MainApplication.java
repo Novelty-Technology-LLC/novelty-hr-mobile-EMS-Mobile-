5e5020e29bc388c20357a104ca609443c0334cda
rj7shakya <shakyarajad1@gmail.com>
2020-11-06 15:38:37 +0545

    package wheel changed

--- android/app/src/main/java/com/noveltyhrmobile/MainApplication.java
+++ android/app/src/main/java/com/noveltyhrmobile/MainApplication.java
@@ -4,7 +4,7 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
-import com.zyu.ReactNativeWheelPickerPackage;
+import com.tron.ReactNativeWheelPickerPackage;
 import io.github.traviskn.rnuuidgenerator.RNUUIDGeneratorPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
