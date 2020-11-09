212a10ff0c0a600b6d998493498eaa0f1c1c6ad6
rj7shakya <shakyarajad1@gmail.com>
2020-11-09 09:52:26 +0545

    unswipe in leave application added

--- src/app/api/uri.tsx
+++ src/app/api/uri.tsx
@@ -1,3 +1,3 @@
-export const BASE_URI = 'https://novelty-hr-api.herokuapp.com/';
-// export const BASE_URI = 'https://b459cdddf5fb.ngrok.io';
+// export const BASE_URI = 'https://novelty-hr-api.herokuapp.com/';
+export const BASE_URI = 'https://414e060c7fb9.ngrok.io';
 export const COSMIC_URI = 'https://api.vitafyhealth.com/api/cosmic/';
