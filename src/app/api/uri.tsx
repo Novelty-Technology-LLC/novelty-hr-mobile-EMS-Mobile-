2984e83d347b5b55c25ebce4a3d888dab60427f5
rj7shakya <shakyarajad1@gmail.com>
2020-11-12 11:47:10 +0545

    picker updated

--- src/app/api/uri.tsx
+++ src/app/api/uri.tsx
@@ -1,3 +1,3 @@
-// export const BASE_URI = 'https://novelty-hr-api.herokuapp.com/';
-export const BASE_URI = 'https://6634bbeedf2c.ngrok.io';
+export const BASE_URI = 'https://novelty-hr-api.herokuapp.com/';
+// export const BASE_URI = 'https://6634bbeedf2c.ngrok.io';
 export const COSMIC_URI = 'https://api.vitafyhealth.com/api/cosmic/';
