43bd5529dc7c1f282c4fed3ede59c8fed7c5a2d8
Lovish Hamal <lovishhamal123@gmail.com>
2020-11-18 21:33:22 +0545

    change

--- src/app/components/leave_screen/otherRequests.tsx
+++ src/app/components/leave_screen/otherRequests.tsx
@@ -52,13 +52,6 @@ const OtherRequests = ({ refresh, params = 0 }: any) => {
                 pastreq = pastreq.concat(req);
                 pastreq.sort((a, b) => (a.updatedAt < b.updatedAt ? 1 : -1));
               } else {
-                if (req.status === 'In Progress') {
-                  const common = progressreq.map((item) => item.id);
-                  if (common.includes(req.id)) return;
-                  myreq = myreq.concat(req);
-                  myreq.sort((a, b) => (a.updatedAt < b.updatedAt ? 1 : -1));
-                  return;
-                }
                 myreq = myreq.concat(req);
                 myreq.sort((a, b) => (a.updatedAt < b.updatedAt ? 1 : -1));
               }
