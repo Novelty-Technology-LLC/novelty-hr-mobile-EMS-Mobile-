a216be1c0173381c17bf454e930030a492402928
Lovish Hamal <lovishhamal123@gmail.com>
2020-11-24 17:02:52 +0545

    profile

--- src/app/screens/profile.tsx
+++ src/app/screens/profile.tsx
@@ -1,16 +1,77 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useContext } from 'react';
+import { View, Text, ScrollView, Image } from 'react-native';
 import { headerText } from '../../assets/styles';
-import { settingStyle as style } from '../../assets/styles/tabs';
-import { ComingSoon, tabHeader as Header } from '../common';
+import { profileStyle as style } from '../../assets/styles/tabs';
+import { tabHeader as Header } from '../common';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import colors from '../../assets/colors';
+import { AuthContext } from '../reducer';
 
 const Profile = () => {
+  const { state } = useContext(AuthContext);
+
+  const uploadImage = () => {
+    console.log('pressed');
+  };
+
   return (
     <View style={style.container}>
-      <Header>
-        <Text style={headerText}>Profile</Text>
-      </Header>
-      <ComingSoon>COMING SOON</ComingSoon>
+      <ScrollView
+        contentContainerStyle={style.container}
+        showsVerticalScrollIndicator={false}
+      >
+        <Header>
+          <Text style={headerText}>Profile</Text>
+        </Header>
+        <View style={style.imageView}>
+          <Image
+            style={style.image}
+            source={{
+              uri: state.user.image_url,
+            }}
+          />
+
+          <TouchableOpacity onPress={uploadImage}>
+            <View style={style.label}>
+              <Icon name="upload" color="white" size={20}></Icon>
+              <Text style={style.labelText}>Upload pic</Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+        <View style={style.infoView}>
+          <View style={style.body}>
+            <Text style={style.heading}>Personal Info</Text>
+            <View style={style.icon}>
+              <Icon name="account-circle" color={colors.primary} size={30} />
+              <Text style={style.text}>
+                {state.user.first_name + ' ' + state.user.last_name}
+              </Text>
+            </View>
+            <View style={style.icon}>
+              <Icon name="account-circle" color={colors.primary} size={30} />
+              <Text style={style.text}>{state.user.gender}</Text>
+            </View>
+            <View style={style.icon}>
+              <Icon name="cake-variant" color={colors.primary} size={30} />
+              <Text style={style.date}>Nov 10, 2020</Text>
+            </View>
+          </View>
+        </View>
+        <View style={style.infoView}>
+          <View style={style.body}>
+            <Text style={style.heading}>Contact Info</Text>
+            <View style={style.icon}>
+              <Icon name="email-newsletter" color={colors.primary} size={30} />
+              <Text style={style.text}>{state.user.email}</Text>
+            </View>
+            <View style={style.icon}>
+              <Icon name="cellphone-basic" color={colors.primary} size={30} />
+              <Text style={style.text}>{state.user.phone}</Text>
+            </View>
+          </View>
+        </View>
+      </ScrollView>
     </View>
   );
 };
