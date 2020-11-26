f1012794d9543fa91a5ebc6853e15bae26db6c96
Lovish Hamal <lovishhamal123@gmail.com>
2020-11-26 15:35:15 +0545

    update

--- src/app/api/uri.tsx
+++ src/app/api/uri.tsx
@@ -1,2 +1,2 @@
-export const BASE_URI = 'https://10.0.2.2:8088';
+export const BASE_URI = 'https://novelty-hr-api.herokuapp.com/';
 export const COSMIC_URI = 'https://api.vitafyhealth.com/api/cosmic/';
