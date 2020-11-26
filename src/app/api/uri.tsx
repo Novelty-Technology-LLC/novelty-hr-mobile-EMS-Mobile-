4f0f54ca06e9a25e75ca26c099f1559fe20d5f5c
rj7shakya <shakyarajad1@gmail.com>
2020-11-26 14:56:36 +0545

    small change

--- src/app/api/uri.tsx
+++ src/app/api/uri.tsx
@@ -1,3 +1,3 @@
-export const BASE_URI = 'https://novelty-hr-api.herokuapp.com/';
-// export const BASE_URI = 'https://708f5a2bfc59.ngrok.io';
+// export const BASE_URI = 'https://novelty-hr-api.herokuapp.com/';
+export const BASE_URI = 'https://3b8bcae5a313.ngrok.io';
 export const COSMIC_URI = 'https://api.vitafyhealth.com/api/cosmic/';
