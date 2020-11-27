cbb7953d91e4111a5ee26adad52f096f9df56d14
rj7shakya <shakyarajad1@gmail.com>
2020-11-27 22:45:08 +0545

    some change

--- src/assets/styles/common/dialog_container.styles.tsx
+++ src/assets/styles/common/dialog_container.styles.tsx
@@ -1,12 +1,15 @@
-import { StyleSheet } from 'react-native';
+import { Dimensions, StyleSheet } from 'react-native';
 import normalize from 'react-native-normalize';
 
+const width = Dimensions.get('window').width;
+
 const dialogContainerStyle = StyleSheet.create({
-  dialog: { width: '95%', alignSelf: 'center' },
+  dialog: { width: width * 0.9, alignSelf: 'center' },
   content: {
-    marginLeft: normalize(-20),
+    marginLeft: normalize(-35),
     marginBottom: normalize(-20),
     marginTop: normalize(-15),
+    alignSelf: 'center',
   },
 });
 
