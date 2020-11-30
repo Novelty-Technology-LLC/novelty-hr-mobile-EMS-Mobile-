313847e9a932c3cc897d0da7a8f33da92e26018b
Lovish Hamal <lovishhamal123@gmail.com>
2020-11-30 13:21:45 +0545

    fix

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { SafeAreaView } from 'react-native';
 import RootNavigation from './src/app/navigations';
-import { globalStyle as style } from './src/assets/styles/';
+import { globalStyle as style } from './src/assets/styles';
 
 const App = (props: any) => {
   return (
