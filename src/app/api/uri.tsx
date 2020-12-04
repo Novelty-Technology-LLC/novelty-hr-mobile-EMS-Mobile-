9cc8c983f438289b47faa945fd8787e84cadc3e1
rj7shakya <shakyarajad1@gmail.com>
2020-12-04 17:40:53 +0545

    some cahnges

--- src/app/api/uri.tsx
+++ src/app/api/uri.tsx
@@ -1,4 +1,3 @@
-export const BASE_URI = 'https://novelty-hr-api.herokuapp.com/';
-// export const BASE_URI = 'https://employee-mgmt-api-dev.herokuapp.com/';
-// export const BASE_URI = 'https://4e1da1d9938b.ngrok.io';
+export const BASE_URI = 'https://employee-mgmt-api-dev.herokuapp.com/';
+// export const BASE_URI = 'https://c9b0e8799e99.ngrok.io';
 export const COSMIC_URI = 'https://api.vitafyhealth.com/api/cosmic/';
