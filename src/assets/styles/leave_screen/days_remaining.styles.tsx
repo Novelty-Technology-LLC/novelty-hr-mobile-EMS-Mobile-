697952d69db6be8b0678018835bd035c6314da4b
rj7shakya <shakyarajad1@gmail.com>
2020-12-09 18:20:23 +0545

    new requested icon

--- src/assets/styles/leave_screen/days_remaining.styles.tsx
+++ src/assets/styles/leave_screen/days_remaining.styles.tsx
@@ -14,6 +14,7 @@ const daysRemainingStyle = StyleSheet.create({
     width: '50%',
     alignItems: 'center',
     paddingBottom: normalize(20),
+    backgroundColor: 'red',
   },
   remaining: {
     fontWeight: '300',
