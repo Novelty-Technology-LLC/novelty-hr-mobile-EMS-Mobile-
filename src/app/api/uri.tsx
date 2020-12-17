f5a2e380d851f273b4956e6341d275d4a2c4561f
Lovish Hamal <lovishhamal123@gmail.com>
2020-12-17 17:16:11 +0545

    update tab

--- src/app/api/uri.tsx
+++ src/app/api/uri.tsx
@@ -1,4 +1,7 @@
-// export const BASE_URI = 'http://127.0.0.1:8088';
 export const BASE_URI = 'https://employee-mgmt-api.herokuapp.com';
+
+// export const BASE_URI = 'https://a8be4dc74871.ngrok.io';
+
 // export const BASE_URI = 'https://974936f79546.ngrok.io';
+
 export const COSMIC_URI = 'https://api.vitafyhealth.com/api/cosmic/';
