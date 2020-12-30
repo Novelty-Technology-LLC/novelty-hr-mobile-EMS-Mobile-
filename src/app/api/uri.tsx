b9aa73679d76de4508b108ea5fc7e573e1993713
rj7shakya <shakyarajad1@gmail.com>
2020-12-30 14:50:41 +0545

    uri change

--- src/app/api/uri.tsx
+++ src/app/api/uri.tsx
@@ -1,3 +1,3 @@
-// export const BASE_URI = 'https://employee-mgmt-api.herokuapp.com';
-export const BASE_URI = 'https://faca222562f4.ngrok.io';
+export const BASE_URI = 'https://employee-mgmt-api.herokuapp.com';
+// export const BASE_URI = 'https://faca222562f4.ngrok.io';
 export const COSMIC_URI = 'https://api.vitafyhealth.com/api/cosmic/';
