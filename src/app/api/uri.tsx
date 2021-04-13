5c78f4c5cd59fcaa2e9f21c659601c2ab48fa90a
rj7shakya <shakyarajad1@gmail.com>
2021-04-13 15:18:13 +0545

    canned message changes

--- src/app/api/uri.tsx
+++ src/app/api/uri.tsx
@@ -1,7 +1,7 @@
 import { Platform } from 'react-native';
 
-// export const BASE_URI = 'https://employee-mgmt-api.herokuapp.com';
-export const BASE_URI = 'https://employee-mgmt-api-dev.herokuapp.com/';
+export const BASE_URI = 'https://employee-mgmt-api.herokuapp.com';
+// export const BASE_URI = 'https://employee-mgmt-api-dev.herokuapp.com/';
 
 // export const BASE_URI = 'http://0b372e60392e.ngrok.io';
 //   Platform.OS === 'android' ? 'http://10.0.2.2:8088' : 'http://127.0.0.1:8088';
