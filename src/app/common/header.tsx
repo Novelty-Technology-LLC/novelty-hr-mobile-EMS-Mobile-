3a5f655614a4c80fb79c1787090027f54b37986e
Devendra <devendra@noveltytechnology.com>
2021-04-17 01:15:48 +0545

    design changes

--- src/app/common/header.tsx
+++ src/app/common/header.tsx
@@ -1,24 +1,35 @@
-import React, { useContext } from 'react';
-import { View, Text } from 'react-native';
-import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
-import { useNavigation } from '@react-navigation/native';
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import React, { useContext } from "react";
+import { View, Text } from "react-native";
+import { TouchableWithoutFeedback } from "react-native-gesture-handler";
+import { useNavigation } from "@react-navigation/native";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 
-import color from '../../assets/colors';
-import { headerStyle as style } from '../../assets/styles';
-import normalize from 'react-native-normalize';
+import color from "../../assets/colors";
+import { headerStyle as style } from "../../assets/styles";
+import normalize from "react-native-normalize";
 
-const header = ({ onPress = null, icon = false, children ,...props}: any) => {
+const header = ({ onPress = null, icon = false, children, ...props }: any) => {
   const navigation = useNavigation();
 
   return (
-    <View style={[style.container,props.container]}>
-      <TouchableWithoutFeedback style={style.textView} disabled={!icon} onPress={icon && (onPress || navigation.goBack)}>
-        {icon && (
-            <Icon name="chevron-left" size={30} color={color.primary} style={{marginLeft:normalize(-8)}}/>
-        )}
-        {children}
-      </TouchableWithoutFeedback>
+    <View style={[style.container, props.container]}>
+      {icon ? (
+        <TouchableWithoutFeedback
+          style={style.textView}
+          disabled={!icon}
+          onPress={icon && (onPress || navigation.goBack)}
+        >
+          <Icon
+            name="chevron-left"
+            size={30}
+            color={color.primary}
+            style={{ marginLeft: normalize(-8) }}
+          />
+          {children}
+        </TouchableWithoutFeedback>
+      ) : (
+        <>{children}</>
+      )}
     </View>
   );
 };
