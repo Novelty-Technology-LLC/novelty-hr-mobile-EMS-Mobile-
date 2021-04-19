4c0fcc501374acd03eece2669efa3c7fd4a33dca
Rajad Shakya <shakyarajad1@gmail.com>
2021-04-19 14:19:53 +0545

    uri change

--- src/app/api/uri.tsx
+++ src/app/api/uri.tsx
@@ -1,8 +1,4 @@
-import { Platform } from "react-native";
-
-// export const BASE_URI = "https://employee-mgmt-api.herokuapp.com";
+export const BASE_URI = "https://employee-mgmt-api.herokuapp.com";
 // export const BASE_URI = 'https://employee-mgmt-api-dev.herokuapp.com/';
-
-export const BASE_URI = "https://68af43c99d27.ngrok.io";
-//   Platform.OS === 'android' ? 'http://10.0.2.2:8088' : 'http://127.0.0.1:8088';
+// export const BASE_URI = "https://68af43c99d27.ngrok.io";
 export const COSMIC_URI = "https://api.vitafyhealth.com/api/cosmic/";
