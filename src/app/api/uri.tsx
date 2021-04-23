f63158413127cae6389e1e93dc3baaa087474046
Rajad Shakya <shakyarajad1@gmail.com>
2021-04-23 22:02:01 +0545

    uri change

--- src/app/api/uri.tsx
+++ src/app/api/uri.tsx
@@ -1,4 +1,4 @@
-// export const BASE_URI = 'https://employee-mgmt-api.herokuapp.com';
+export const BASE_URI = 'https://employee-mgmt-api.herokuapp.com';
 // export const BASE_URI = 'https://employee-mgmt-api-dev.herokuapp.com/';
-export const BASE_URI = 'https://a80b68f4050f.ngrok.io';
+// export const BASE_URI = 'https://a80b68f4050f.ngrok.io';
 export const COSMIC_URI = 'https://api.vitafyhealth.com/api/cosmic/';
