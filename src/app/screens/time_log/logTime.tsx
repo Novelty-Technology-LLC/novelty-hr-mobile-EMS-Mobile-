b8cf933748ace81ecd0dc94c15c67c7ed75156f0
roch <roch@noveltytechnology.com>
2021-05-12 18:52:02 +0545

    quick fix

--- src/app/screens/time_log/logTime.tsx
+++ src/app/screens/time_log/logTime.tsx
@@ -26,7 +26,6 @@ import { useNavigation } from '@react-navigation/native';
 import colors from '../../../assets/colors';
 import { TimeLogContext } from '../../reducer';
 import { checkAndReplace, momentdate } from '../../utils/momentDate';
-import moment from 'moment';
 
 const LogTime = ({ route }: any) => {
   const navigation = useNavigation();
