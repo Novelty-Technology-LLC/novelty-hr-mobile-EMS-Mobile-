629b55a047d97db1334cbb15b55e49cc6e8a3e04
Lovish Hamal <lovishhamal123@gmail.com>
2021-05-24 10:54:19 +0545

    fixed old data in graph error

--- src/app/api/uri.tsx
+++ src/app/api/uri.tsx
@@ -1,4 +1,4 @@
-// export const BASE_URI = 'https://employee-mgmt-api.herokuapp.com/';
-export const BASE_URI = 'https://ab3e52533bdb.ngrok.io';
+export const BASE_URI = 'https://employee-mgmt-api.herokuapp.com/';
+// export const BASE_URI = 'https://ab3e52533bdb.ngrok.io';
 // export const BASE_URI = 'https://29bff8a64a20.ngrok.io';
 export const COSMIC_URI = 'https://api.vitafyhealth.com/api/cosmic/';
