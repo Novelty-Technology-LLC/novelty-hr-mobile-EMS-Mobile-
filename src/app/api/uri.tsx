62cc4b9bbccacb715fe898a8eefa3ea607da5a17
Lovish Hamal <lovishhamal@Lovishs-MacBook-Pro.local>
2021-11-23 13:15:36 +0545

    changed url

--- src/app/api/uri.tsx
+++ src/app/api/uri.tsx
@@ -1,2 +1,2 @@
-export const BASE_URI = 'http://127.0.0.1:8088/';
+export const BASE_URI = 'https://employee-mgmt-api.herokuapp.com/';
 export const COSMIC_URI = 'https://api.vitafyhealth.com/api/cosmic/';
