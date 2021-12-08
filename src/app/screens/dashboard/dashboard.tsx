f0f748bcf61b435e80ab202e5f63c480695d1e9b
Devendra <devendra@noveltytechnology.com>
2021-12-08 12:25:15 +0545

    text changes

--- src/app/screens/dashboard/dashboard.tsx
+++ src/app/screens/dashboard/dashboard.tsx
@@ -191,7 +191,7 @@ const DashBoard = () => {
             <List
               list={{
                 module: "Announcements",
-                message: "No Upcomming Announcements",
+                message: "No Upcoming Announcements",
                 items: announcements,
                 detailRoute: "announcementsListing",
               }}
