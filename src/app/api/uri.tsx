ac0e5b0c556305d377a144f59ba0674cdcf6b245
Lovish Hamal <lovishhamal@Lovishs-MacBook-Pro.local>
2021-12-15 20:37:17 +0545

    updated design

--- src/app/api/uri.tsx
+++ src/app/api/uri.tsx
@@ -1,2 +1,2 @@
-export const BASE_URI = 'https://employee-mgmt-api.herokuapp.com/';
+export const BASE_URI = 'https://employee-mgmt-api-dev.herokuapp.com/';
 export const COSMIC_URI = 'https://api.vitafyhealth.com/api/cosmic/';
