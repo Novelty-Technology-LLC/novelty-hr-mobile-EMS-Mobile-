5e2bf699c5479e6a29a19634224c26c4ca1874fe
Lovish Hamal <lovish@Lovishs-MacBook-Pro.local>
2022-06-10 12:00:39 +0545

    fixed leave quota

--- src/app/api/uri.tsx
+++ src/app/api/uri.tsx
@@ -1,3 +1,2 @@
-export const BASE_URI =
-  'https://a69d-2407-5200-400-9b92-8160-8291-ca0b-84a7.ngrok.io/';
+export const BASE_URI = 'https://employee-mgmt-api-dev.herokuapp.com/';
 export const COSMIC_URI = 'https://api.vitafyhealth.com/api/cosmic/';
