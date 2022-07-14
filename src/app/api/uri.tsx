890793ec5a54a7d3c7505523869f3d4d80d9eec4
Lovish Hamal <lovish@noveltytechnology.com>
2022-07-14 14:18:43 +0545

    update

--- src/app/api/uri.tsx
+++ src/app/api/uri.tsx
@@ -1,2 +1,2 @@
-export const BASE_URI = "https://dev-api-ems.noveltytechnology.com/";
+export const BASE_URI = "http://127.0.0.1:8088/";
 export const COSMIC_URI = "https://api.vitafyhealth.com/api/cosmic/";
