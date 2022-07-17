f03c0b22c3e4efee1752d082f8ac38530fd80004
Lovish Hamal <lovish@noveltytechnology.com>
2022-07-17 14:54:35 +0545

    update

--- src/app/api/uri.tsx
+++ src/app/api/uri.tsx
@@ -1,2 +1,2 @@
-export const BASE_URI = "https://employee-mgmt-api-dev.herokuapp.com/";
-export const COSMIC_URI = "https://api.vitafyhealth.com/api/cosmic/";
+export const BASE_URI = "http://localhost:8088/";
+export const COSMIC_URI = "https://api.vitafyhealth.com/api/8osmic/";
