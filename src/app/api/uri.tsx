359d68ae4cea935ddc043780dc2825e6b0c52918
Lovish Hamal <lovish@Lovishs-MacBook-Pro.local>
2022-07-28 17:02:58 +0545

    changed baseurl

--- src/app/api/uri.tsx
+++ src/app/api/uri.tsx
@@ -1,2 +1,2 @@
-export const BASE_URI = "https://ems-api.noveltytechnology.com/api/";
+export const BASE_URI = "https://ems-api.noveltytechnology.com";
 export const COSMIC_URI = "https://api.vitafyhealth.com/api/cosmic/";
--- src/app/screens/dashboard/dashboard.tsx
+++ src/app/screens/dashboard/dashboard.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState, useEffect } from 'react'
+import React, { useContext, useState, useEffect } from "react";
 import {
   Text,
   View,
@@ -9,9 +9,9 @@ import {
   RefreshControl,
   TouchableOpacity,
   BackHandler,
-} from 'react-native'
-import { AuthContext } from '../../reducer'
-import { dashboardStyle as ds, headerTxtStyle } from '../../../assets/styles'
+} from "react-native";
+import { AuthContext } from "../../reducer";
+import { dashboardStyle as ds, headerTxtStyle } from "../../../assets/styles";
 import {
   Cards,
   header as Header,
@@ -20,160 +20,163 @@ import {
   snackBarMessage,
   snackErrorBottom,
   snackErrorTop,
-} from '../../common'
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
-import colors from '../../../assets/colors'
-import { getToday } from '../../utils'
-import { createWork, getWork, getDashboard, getRequest } from '../../services'
-import moment from 'moment'
-import normalize from 'react-native-normalize'
-import { DashboardCardPlaceholder } from '../../common'
-import { getCurrentRouteName, navigate } from '../../utils/navigation'
-import { time } from '../../utils/listtranform'
+} from "../../common";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
+import colors from "../../../assets/colors";
+import { getToday } from "../../utils";
+import { createWork, getWork, getDashboard, getRequest } from "../../services";
+import moment from "moment";
+import normalize from "react-native-normalize";
+import { DashboardCardPlaceholder } from "../../common";
+import { getCurrentRouteName, navigate } from "../../utils/navigation";
+import { time } from "../../utils/listtranform";
 
 const DashBoard = () => {
-  const { state } = useContext(AuthContext)
-  const [toggle, setToggle] = useState(false)
-  const [id, setId] = useState(0)
-  const [userId, setUserId] = useState(0)
-  const [loading, setLoading] = useState(false)
-  const [leaveStatus, setLeaveStatus] = useState(false)
-  const [announcementLoading, setAnnouncementLoading] = useState(false)
-  const [listData, setListData] = useState([])
-  const [announcements, setAnnouncements] = useState([])
-  const [refreshing, setRefreshing] = useState(false)
-  const [cardLoading, setCardLoading] = useState(true)
+  const { state } = useContext(AuthContext);
+  const [toggle, setToggle] = useState(false);
+  const [id, setId] = useState(0);
+  const [userId, setUserId] = useState(0);
+  const [loading, setLoading] = useState(false);
+  const [leaveStatus, setLeaveStatus] = useState(false);
+  const [announcementLoading, setAnnouncementLoading] = useState(false);
+  const [listData, setListData] = useState([]);
+  const [announcements, setAnnouncements] = useState([]);
+  const [refreshing, setRefreshing] = useState(false);
+  const [cardLoading, setCardLoading] = useState(true);
 
   const onRefresh = React.useCallback(() => {
-    setRefreshing(true)
-  }, [])
+    setRefreshing(true);
+  }, []);
 
   useEffect(() => {
-    BackHandler.addEventListener('hardwareBackPress', () => {
-      if (getCurrentRouteName() === 'dashboard') {
-        BackHandler.exitApp()
+    BackHandler.addEventListener("hardwareBackPress", () => {
+      if (getCurrentRouteName() === "dashboard") {
+        BackHandler.exitApp();
       }
-    })
+    });
     return () => {
-      BackHandler.removeEventListener('hardwareBackPress', BackHandler.exitApp)
-    }
-  }, [])
+      BackHandler.removeEventListener("hardwareBackPress", BackHandler.exitApp);
+    };
+  }, []);
   const fetchWork = async () => {
     try {
-      setLoading(true)
+      setLoading(true);
       const res: any = await getWork({
         user_id: state?.user?.id,
-        date: moment().format('YYYY-MM-DD'),
-      })
+        date: moment().format("YYYY-MM-DD"),
+      });
 
-      setId(res?.data?.data[0].id ?? null)
+      setId(res?.data?.data[0].id ?? null);
       // setUserId(res?.data?.data[0].user_id ?? null);
 
-      setToggle(+res?.data?.data[0].status === 1 ? true : false)
-      setLoading(false)
+      setToggle(+res?.data?.data[0].status === 1 ? true : false);
+      setLoading(false);
     } catch (error) {
-      setLoading(false)
+      setLoading(false);
     }
-  }
+  };
   useEffect(() => {
-    state?.user?.id && fetchWork()
-  }, [state?.user?.id])
+    state?.user?.id && fetchWork();
+  }, [state?.user?.id]);
   useEffect(() => {
-    state?.user?.id && fetchLeave()
-  }, [state?.user?.id])
+    state?.user?.id && fetchLeave();
+  }, [state?.user?.id]);
   const fetchAnnouncements = async () => {
     try {
-      var response: any = await getRequest('/webportal/announcements', {
+      var response: any = await getRequest("/webportal/announcements", {
         limit: 3,
-      })
+      });
 
-      setAnnouncements(response)
-    } catch (error) { }
-  }
+      setAnnouncements(response);
+    } catch (error) {}
+  };
   const fetchLeave = async () => {
     try {
-      var response: any = await getRequest('/leave', {})
-      var date = moment(new Date()).format('ddd MMM D YYYY')
+      var response: any = await getRequest("/leave", {});
+      var date = moment(new Date()).format("ddd MMM D YYYY");
 
       var todayLeave = response.find(function (element: any) {
         return (
           element.requestor_id === state?.user.id &&
           moment(date).isSame(element.leave_date.startDate)
-        )
-      })
+        );
+      });
 
-      if (todayLeave.status === 'Approved') {
-        setLeaveStatus(true)
+      if (todayLeave.status === "Approved") {
+        setLeaveStatus(true);
       } else {
-        setLeaveStatus(false)
+        setLeaveStatus(false);
       }
-    } catch (error) { }
-  }
+    } catch (error) {}
+  };
   useEffect(() => {
-    ; (async () => {
+    (async () => {
       try {
-        setAnnouncementLoading(true)
+        setAnnouncementLoading(true);
 
-        setCardLoading(true)
-        const data: any = await getDashboard()
-        await fetchLeave()
+        setCardLoading(true);
+        const data: any = await getDashboard();
+        console.log("data", data);
 
-        await fetchAnnouncements()
+        await fetchLeave();
 
-        setAnnouncementLoading(false)
-        setListData(data)
-        setRefreshing(false)
-        setCardLoading(false)
+        await fetchAnnouncements();
+
+        setAnnouncementLoading(false);
+        setListData(data);
+        setRefreshing(false);
+        setCardLoading(false);
       } catch (error) {
-        setRefreshing(false)
+        console.log("error", error);
+
+        setRefreshing(false);
       }
-    })()
-  }, [refreshing])
+    })();
+  }, [refreshing]);
 
   const ToggleWork = async () => {
-
     if (leaveStatus) {
-      showToast('You are currently on leave', false)
+      showToast("You are currently on leave", false);
     } else {
       try {
-        setLoading(true)
+        setLoading(true);
         const data = {
           id,
           date: getToday(),
           user_id: state?.user?.id,
           status: !toggle ? 1 : 0,
-        }
-        const res: any = await createWork(data)
-        res?.data?.data?.id && setId(res?.data?.data?.id)
+        };
+        const res: any = await createWork(data);
+        res?.data?.data?.id && setId(res?.data?.data?.id);
         if (res?.data?.data?.message) {
-          showToast(res?.data?.data?.message, false)
-          setLoading(false)
+          showToast(res?.data?.data?.message, false);
+          setLoading(false);
         } else if (res?.data?.status === 200) {
-          showToast('Successfully changed status.')
-          setToggle(!toggle)
+          showToast("Successfully changed status.");
+          setToggle(!toggle);
           let newList: any = listData.find(
-            (item: any) => item?.detailRoute === '/employee',
-          )
+            (item: any) => item?.detailRoute === "/employee"
+          );
           newList.items.map((item: any) => {
-            if (item?.subTitle === 'Working from Home') {
-              item.title = !toggle ? +item.title + 1 : +item.title - 1
+            if (item?.subTitle === "Working from Home") {
+              item.title = !toggle ? +item.title + 1 : +item.title - 1;
             }
-          })
-          setLoading(false)
+          });
+          setLoading(false);
         }
       } catch (error) {
-        showToast('Something went wrong', false)
-        setLoading(false)
+        showToast("Something went wrong", false);
+        setLoading(false);
       }
     }
-  }
+  };
 
   return (
     <View style={ds.safeArea}>
       <Header icon={false} container={{ paddingVertical: normalize(4.076) }}>
         <View style={ds.headerContainer}>
           <Text style={headerTxtStyle.headerText}>DASHBOARD</Text>
-          <TouchableOpacity onPress={() => navigate('Profile')}>
+          <TouchableOpacity onPress={() => navigate("Profile")}>
             <Image source={{ uri: state?.user?.image_url }} style={ds.image} />
           </TouchableOpacity>
         </View>
@@ -239,14 +242,14 @@ const DashBoard = () => {
           )}
         </View>
         <View style={{ height: 20 }} />
-        <View style={{ width: '100%', paddingBottom: 25 }}>
+        <View style={{ width: "100%", paddingBottom: 25 }}>
           {!announcementLoading ? (
             <List
               list={{
-                module: 'Announcements',
-                message: 'No Upcoming Announcements',
+                module: "Announcements",
+                message: "No Upcoming Announcements",
                 items: announcements,
-                detailRoute: 'announcementsListing',
+                detailRoute: "announcementsListing",
               }}
             />
           ) : (
@@ -255,7 +258,7 @@ const DashBoard = () => {
         </View>
       </ScrollView>
     </View>
-  )
-}
+  );
+};
 
-export { DashBoard }
+export { DashBoard };
