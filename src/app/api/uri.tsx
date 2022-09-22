a9e1930abc1bf3c8e82b77e8d5b5f1fa01ddd8ff
Lovish Hamal <lovish@Lovishs-MacBook-Pro.local>
2022-09-22 18:23:10 +0545

    changed base url

--- src/app/api/uri.tsx
+++ src/app/api/uri.tsx
@@ -1,2 +1,5 @@
-export const BASE_URI = "https://dev-api-ems.noveltytechnology.com/";
+export const BASE_URI = "https://ems-api.noveltytechnology.com/";
 export const COSMIC_URI = "https://api.vitafyhealth.com/api/cosmic/";
+
+// DEV = "https://dev-api-ems.noveltytechnology.com/";
+// PROD = "https://ems-api.noveltytechnology.com/api";
