258dcc2672bd9f5a43508c50dc19a33bac8db20e
Lovish Hamal <lovish@Lovishs-MacBook-Pro.local>
2022-09-29 21:24:37 +0545

    update from devv

--- src/app/api/uri.tsx
+++ src/app/api/uri.tsx
@@ -1,5 +1,6 @@
 const DEV = "https://dev-api-ems.noveltytechnology.com/";
 const PROD = "https://ems-api.noveltytechnology.com/";
+const LOCAL = "http://127.0.0.1:8088/";
 
-export const BASE_URI = PROD;
+export const BASE_URI = LOCAL;
 export const COSMIC_URI = "https://api.vitafyhealth.com/api/cosmic/";
