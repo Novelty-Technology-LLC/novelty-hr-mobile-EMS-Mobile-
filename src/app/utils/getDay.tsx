6b416469314a31459cd6d9467e1cf401ba9247db
Suprimshk <suprim@noveltytechnology.com>
2023-01-10 15:20:08 +0545

    pulled from dev

--- src/app/utils/getDay.tsx
+++ src/app/utils/getDay.tsx
@@ -3,18 +3,14 @@ import { dateStringMapper } from "./dateMapper";
 export default getDay = (item) => {
   if (!item?.start_date) return "";
   let day = new Date(item?.start_date).toString().substr(3, 7);
-
   let dayRange = dateStringMapper(item?.start_date, item?.end_date);
-
-  let dayType = item?.start_date?.slice(3, 10) - +item?.end_date.slice(8, 10);
-
+  let dayType = item?.start_date.slice(3, 10) - +item?.end_date.slice(8, 10);
   return {
     day,
     dayRange,
     dayType,
   };
 };
-
 export const responseDay = (date) =>
   new Date(date.updatedAt).toString().substr(3, 7);
 
