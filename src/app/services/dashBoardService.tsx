9b2ebdd4e4630ad5704e63b033f7d65877a6ac55
Lovish Hamal <lovish@noveltytechnology.com>
2023-02-03 17:00:03 +0545

    update

--- src/app/services/dashBoardService.tsx
+++ src/app/services/dashBoardService.tsx
@@ -1,3 +1,4 @@
+import moment from "moment";
 import { api } from "../api/api";
 import { getToday, getYesterday } from "../utils";
 
@@ -6,7 +7,7 @@ const getDashboard = (user_id: number) => {
     try {
       const res = await api.get("/dashboard", {
         params: {
-          todayDate: getToday("YYYY-MM-DD"),
+          todayDate: new Date(moment(new Date()).format("YYYY-MM-DD")),
           user_id,
         },
       });
