1f7af2fcd35909840907930795fc657a35036613
Pragyan Shrestha <stha.pragyan.17@gmail.com>
2023-03-08 11:22:52 +0545

    uri changed

--- src/app/api/uri.tsx
+++ src/app/api/uri.tsx
@@ -1,6 +1,6 @@
 const DEV = "https://dev-api-ems.noveltytechnology.com/";
 const PROD = "https://ems-api.noveltytechnology.com/";
-const LOCAL = "https://e1be-103-163-182-52.ngrok.io";
+const LOCAL = "https://584f-111-119-49-58.ngrok.io";
 
-export const BASE_URI = LOCAL;
+export const BASE_URI = DEV;
 export const COSMIC_URI = "https://api.vitafyhealth.com/api/cosmic/";
