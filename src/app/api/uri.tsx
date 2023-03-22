c7eb5a39c46914de12f8c5628f77527104d7314c
pragyann_ <pragyan@noveltytechnology.com>
2023-03-22 11:35:14 +0545

    base url changed

--- src/app/api/uri.tsx
+++ src/app/api/uri.tsx
@@ -2,5 +2,5 @@ const DEV = "https://dev-api-ems.noveltytechnology.com/";
 const PROD = "https://ems-api.noveltytechnology.com/";
 const LOCAL = "https://9f7a-103-186-197-218.ngrok.io";
 
-export const BASE_URI = LOCAL;
+export const BASE_URI = DEV;
 export const COSMIC_URI = "https://api.vitafyhealth.com/api/cosmic/";
