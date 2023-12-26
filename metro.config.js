aefd57d4db2e6f10d84202bfb4a4f32f2250df8a
Lovish Hamal <lovish@noveltytechnology.com>
2023-12-26 17:51:45 +0545

    fix: fixes

--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,6 @@
 const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const MetroConfig = require('@ui-kitten/metro-config');
+const defaultConfig = require('metro-config/src/defaults').getDefaultValues();
 
 /**
  * Metro configuration
@@ -6,6 +8,25 @@ const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
  *
  * @type {import('metro-config').MetroConfig}
  */
-const config = {};
+
+const evaConfig = {
+  evaPackage: '@eva-design/eva',
+};
+
+const config = MetroConfig.create(evaConfig, {
+  transformer: {
+    babelTransformerPath: require.resolve('react-native-svg-transformer'),
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: false,
+      },
+    }),
+  },
+  resolver: {
+    assetExts: defaultConfig.resolver.assetExts.filter(ext => ext !== 'svg'),
+    sourceExts: [...defaultConfig.resolver.sourceExts, 'svg'],
+  },
+});
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
