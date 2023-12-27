fb8ae2a89a5cc45ff093199f96fcfb305993cb02
Lovish Hamal <lovish@noveltytechnology.com>
2023-12-27 14:14:27 +0545

    fix: fixed apple sign in issue

--- src/app/api/uri.tsx
+++ src/app/api/uri.tsx
@@ -2,6 +2,6 @@ const DEV = "https://dev-api-ems.noveltytechnology.com/";
 const PROD = "https://ems-api.noveltytechnology.com/";
 const LOCAL = "https://005f-124-41-240-94.ngrok.io";
 
-export const isDev = false; // change to false if uri is in PROD
-export const BASE_URI = PROD;
+export const isDev = true; // change to false if uri is in PROD
+export const BASE_URI = DEV;
 export const COSMIC_URI = "https://api.vitafyhealth.com/api/cosmic/";
