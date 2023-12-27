129bd3eb196da76f2d18043cb277fddbc877ca00
Lovish Hamal <lovish@noveltytechnology.com>
2023-12-27 11:29:54 +0545

    fix: fixed notification issue

--- src/app/utils/googleConfig.tsx
+++ src/app/utils/googleConfig.tsx
@@ -3,7 +3,7 @@ import {GoogleSignin} from '@react-native-google-signin/google-signin';
 const GoogleConfig = () =>
   GoogleSignin.configure({
     webClientId:
-      '1058123517066-ncsq4l5rj44mro0g81cqid6uuv3jvpvt.apps.googleusercontent.com',
+      '1058123517066-dmp3h1jjvc40bdfkqi4n1isj8f9nmhjf.apps.googleusercontent.com',
     iosClientId:
       '1058123517066-9rohggcgu4imr4ni1ubt02csvbc2nen2.apps.googleusercontent.com',
     offlineAccess: true,
